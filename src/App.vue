7db212691f5ea2c14b3364369a8275411e7a84c3
Travis Ray <tripp.machine@gmail.com>
2021-06-07 02:21:17 -0400

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-center items-center flex-col">
+    <img class="mb-6" alt="Rideclub Logo" src="./assets/logo.svg" />
+    <p>
+      Rideclub is coming... Sit tight. We're bringing the heat.
+    </p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
